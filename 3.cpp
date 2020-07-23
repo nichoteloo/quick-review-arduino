91f468dfbdc38d25677f87d854b66b6abc7777ab
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2020-07-23 23:10:47 +0700

    3
    for loop and multi LED circuit

---
+++ 3.cpp
@@ -0,0 +1,28 @@
+int redLEDPin = 9;
+int yellowREDPin = 10;
+int redOnTime = 250;
+int redOffTime = 250;
+int yellowOnTime = 250;
+int yellowOffTime = 250;
+int numRedBlink = 10;
+
+void setup() {
+    pinMode (redLEDPin, OUTPUT);
+    pinMode (yellowLEDPin, OUTPUT);
+}
+
+void loop() {
+
+    for(i=1; i<=numRedBlink; i=i+1) {
+        digitalWrite(redLEDPin, HIGH);
+        delay(redOnTime);
+        digitalWrite(redLEDPin, LOW);
+        delay(redOffTime);
+    }
+
+    digitalWrite(yelloLEDPin, HIGH);
+    delay(yelloOnTime);
+    digitalWrite(yelloLEDPin, LOW);
+    delay(yelloOffTime);
+    
+}
