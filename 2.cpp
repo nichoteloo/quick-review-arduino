2879dc9e884069289229a951faa72e8e37b1d150
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2020-07-23 22:21:49 +0700

    2
    by LED attendance

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+int redLED = 9;
+int onTime = 100;
+int offTime = 900;
+
+void setup() {
+    pinMode (redLED, OUTPUT);
+}
+
+void loop() {
+    digitalWrite (redLED, HIGH);
+    delay(onTime);
+    digitalWrite (redLED, LOW);
+    delay(offTime);
+}
