2476f5db0cd160c3b4f4bd363d4999e060bd6efa
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2020-07-24 12:48:32 +0700

    3
    set int i datatype and configure port

--- 3.cpp
+++ 3.cpp
@@ -8,20 +8,27 @@ int numRedBlink = 10;
 int numYellowBlink = 10;
 
 void setup() {
+    Serial.begin(9600); // setting up port initialization, baud at the port must be 9600
     pinMode (redLEDPin, OUTPUT);
     pinMode (yellowLEDPin, OUTPUT);
 }
 
 void loop() {
 
-    for(i=1; i<=numRedBlink; i=i+1) {
+    Serial.println("The RED LED is Blinking");  // title
+
+    for(int i=1; i<=numRedBlink; i=i+1) {
+        Serial.print("   You are on Blink # : ");
+        Serial.println(i);  // print out in the monitor
         digitalWrite(redLEDPin, HIGH);
         delay(redOnTime);
         digitalWrite(redLEDPin, LOW);
         delay(redOffTime);
     }
 
-    for(i=1; i<=numYellowBlink; i=i+1) {
+    Serial.println(" ");   // enter section
+
+    for(int i=1; i<=numYellowBlink; i=i+1) {
     digitalWrite(yelloLEDPin, HIGH);
     delay(yelloOnTime);
     digitalWrite(yelloLEDPin, LOW);
